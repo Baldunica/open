f9cdee386c3cf435513720ecec3013b346116096
Wogoxi <163595647+Wogoxi@users.noreply.github.com>
2024-03-16 01:08:05 +0300

    mdaa

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense, startTransition } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import Scene from "./Loader";
 import { EarthCanvas } from "./canvas";
 
 const Hero = () => {
